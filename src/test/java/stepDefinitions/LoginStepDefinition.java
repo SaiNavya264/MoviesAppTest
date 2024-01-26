040f0b54618d2c509b8300073b7491533f59a25e
SaiNavya264 <148525882+SaiNavya264@users.noreply.github.com>
2024-01-26 09:28:58 +0530

    Update LoginStepDefinition.java

--- src/test/java/stepDefinitions/LoginStepDefinition.java
+++ src/test/java/stepDefinitions/LoginStepDefinition.java
@@ -22,34 +22,29 @@ public class LoginStepDefinition {
     }
     @When("user enters username")
     public void user_enters_username() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
+       
 
         driver.findElement(By.xpath("//*[@id=\"usernameInput\"]")).sendKeys("rahul");
     }
     @When("user enters password")
     public void user_enters_password() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
+        
         driver.findElement(By.xpath("//*[@id=\"passwordInput\"]")).sendKeys("rahul@2021");
     }
     @When("user clicks login button")
     public void user_clicks_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
+       
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/form/button")).click();
     }
     @When("user clicks Account page")
     public void user_clicks_account_page() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav/div[2]/button[2]/img")).click();
     }
     @Then("user clicks logout")
     public void user_clicks_logout() {
-        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
+    
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div[3]/button")).click();
     }
